640dbc250b173dbb6a1f6b8881a7f815b395d4f1
Adamu Sulaiman <sulaimancodes2235@gmail.com>
2025-12-02 14:46:24 +0100

    refactor!: Switched to a class-based structure

--- calculator.py
+++ calculator.py
@@ -1,11 +1,13 @@
 # calculator.py 
-def add(a, b): 
-    """This function adds two numbers""" 
-    return a + b 
-def subtract(a, b): 
-    """This function subtracts two numbers""" 
-    return a - b 
-print("Welcome to the Super Simple Calculator!") 
-print("Version: 1.1.0") # Update version number 
-print("Result of add(2, 2) is:", add(2, 2)) 
-print("Result of subtract(5, 3) is:", subtract(5, 3)) # Test new feature
+class Calculator: 
+    def add(self, a, b): 
+        """This method adds two numbers""" 
+        return a + b 
+    def subtract(self, a, b): 
+        """This method subtracts two numbers""" 
+        return a - b 
+calc = Calculator() 
+print("Welcome to the Advanced Calculator!") 
+print("Version: 2.0.0") # Update version number 
+print("Result of add(2, 2) is:", calc.add(2, 2)) 
+print("Result of subtract(5, 3) is:", calc.subtract(5, 3)) 
